5423ba161f7e1776e8f1b8c76008fbdccbefa848
LinneaKristoffersson <linnea.kristoffersson@elev.ga.lbs.se>
2026-03-20 11:02:14 +0100

    start of js
    Påbörjan av javascript steg 3-6

--- PieClicker.js
+++ PieClicker.js
@@ -1,3 +1,4 @@
+//pie hovver skugga effekt
 const pie = document.querySelector("#pie-Clicker img");
 
 pie.addEventListener("mouseenter", () => {
@@ -7,3 +8,27 @@ pie.addEventListener("mouseenter", () => {
 pie.addEventListener("mouseleave", () =>{
     pie.src = "pie.png";
 });
+
+
+
+//spel data
+let score = 0;
+let pointsPerClick = 1;
+
+//html element
+const scoreElement = document.getElementById("score");
+const pie_ClickerElemnt = document.getElementById("pie-Clicker");
+const pointsPerClickElement = document.getElementById("points-per-click");
+
+//funtction display
+function updateDisplay(){
+    scoreElement.textContent = score;
+    pointsPerClickElement.textContent = pointsPerClick;
+
+}
+
+//function handel click
+function handelClick(){
+    score = score + pointsPerClick
+    updateDisplay();
+}
